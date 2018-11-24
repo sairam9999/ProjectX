8d2b254545453b6851fa82b8629efbaf28971b0e
sairam9999 <vsairamanroid@gmail.com>
2018-11-23 21:03:56 -0500

    Added Permissions in AndroidManifest and Updated MIN SDK level

--- app/src/main/java/com/example/sairam/boomerang/MainActivity.java
+++ app/src/main/java/com/example/sairam/boomerang/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -66,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                             100);
                 }
 
-//
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (cameraIntent.resolveActivity(getPackageManager()) != null) {
                     // Create the File where the photo should go
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
