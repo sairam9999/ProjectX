29cee16522cfd1e77cf3e6e0c45d919cc07eade5
sairam9999 <vsairamanroid@gmail.com>
2018-11-25 18:53:57 -0500

    Provided the functionality for Camera

---
+++ app/src/main/java/com/example/sairam/boomerang/CameraActivity.java
@@ -0,0 +1,159 @@
+package com.example.sairam.boomerang;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//import static com.example.sairam.boomerang.MainActivity.REQUEST_IMAGE_CAPTURE;
+
+public class CameraActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Context context = CameraActivity.this;
+
+    Button button_snapImage;
+    ImageView imageView_preview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        button_snapImage = (Button) findViewById(R.id.button_snapImage);
+        imageView_preview = (ImageView) findViewById(R.id.imageView_preview);
+
+        button_snapImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 123);
+
+                } else {
+                    ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA},
+                            100);
+                }
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 123);
+
+                } else {
+                    ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA},
+                            100);
+                }
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+//                        Log.i(TAG, "IOException");
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+
+            }
+
+            private File createImageFile() throws IOException {
+                String mCurrentPhotoPath;
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "_";
+                File storageDir = Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES);
+                File image = File.createTempFile(
+                        imageFileName,  // prefix
+                        ".jpg",         // suffix
+                        storageDir      // directory
+                );
+
+                // Save a file: path for use with ACTION_VIEW intents
+                mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+                return image;
+
+            }
+        });
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/Loop");
+        boolean success = true;
+        if (!file.exists()) {
+            Toast.makeText(getApplicationContext(), "Directory does not exist, create it",
+                    Toast.LENGTH_SHORT).show();
+        }
+        if (success) {
+            Toast.makeText(getApplicationContext(), "Directory Created",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Failed Error",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 123:
+
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                //  imageView_preview.setImageBitmap(bitmap);
+
+                break;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 123);
+
+                } else {
+                    Toast.makeText(context, "Permission denined by user", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+}
+
